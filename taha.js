23aefae3c4580ced1ba0d58078d3782cc4a46feb
programmer taha <92051156+ronaldinho12345@users.noreply.github.com>
2022-02-04 20:22:49 +0200

    Add files via upload

---
+++ taha.js
@@ -0,0 +1,31 @@
+var x = 1
+y=2
+z=x+y,
+console.log(z);
+console.log("my name ius taha kourane")
+console.log(taha.repeat(20))
+((name,email)=>{
+    this.name=name,
+    this.email=email
+})
+let Users = new users("tahakourani","tahakorane88gmsil.com")
+["my name is taha","my favorite carrier is hking","taha"]=Users;
+console.log(Users)
+console.log('taha'.repeat(20))
+((taha)=>{
+    firstname=name,
+    lastname=NamedNodeMap;
+})
+console.log(firstname)
+let users = new user("taha kourani","taha")
+console.log(this.users)
+const taha= new Promise.prototype;
+console.log(taha)
+((taha,password)=>{
+    password=",",
+    username="my name is taha kourane"
+    if(username!=taha){
+     resolve("taha")
+    }
+})
+//my name is taha kourane and i am programmer so i wnt to make some values to solve thid quetsions
